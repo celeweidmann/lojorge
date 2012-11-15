f9e2529a9a60ee5877f81fbe189beeb0b19b3ff2
cele <celeweidmann@gmail.com>
2012-11-15 19:23:44 -0300

    NauticasController.

---
+++ app/Controller/NauticasController.php
@@ -0,0 +1,13 @@
+<?php
+class NauticasController extends AppController {
+    var $name = 'Nauticas';
+    var $helpers = array('Html', 'Form');
+    
+    public function index()
+    {
+        
+    }
+    
+}
+    
+?>
