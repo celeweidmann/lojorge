c99e628660b92a120322933ee8ced72b91f8c50a
cele <celeweidmann@gmail.com>
2012-11-22 00:28:03 -0300

    valido los datos antes de enviar el mensaje

--- app/Controller/ContactosController.php
+++ app/Controller/ContactosController.php
@@ -8,73 +8,46 @@ class ContactosController extends AppController {
     public function index(){
 
        if ($this->request->is('post')) {
-                 
+            
+            //Creo un nuevo CakeEmail usando la plantilla gmail.     
             $email = new CakeEmail('gmail');
                    
+                   
             if(isset($this->data['Contacto'])){
+
+                $this->Contacto->set($this->data);
                 
-                //Armo el mensaje para enviar al mail.   
-                $mensaje = 'Enviado por: '.$this->data['Contacto']['nombre']."\n".
-                           'Mail de contacto: '.$this->data['Contacto']['email']."\n".
-                           'Consulta: '.$this->data['Contacto']['message'];
+                //Valido los datos del formulario
+                if($this->Contacto->validates()){
+                                
+                    //Armo el mensaje para enviar al mail.   
+                    $mensaje = 'Enviado por: '.$this->data['Contacto']['nombre']."\n".
+                               'Mail de contacto: '.$this->data['Contacto']['email']."\n".
+                               'Consulta: '.$this->data['Contacto']['mensaje'];
                 
-                //Envia la consulta al mail de cabañas.
-                if($this->data['Contacto']['consulta'] == 0){
-                    $email->to('celeweidmann@gmail.com');
-                } 
-                //Envia la consulta al mail de Almacén Náutico
-                elseif ($this->data['Contacto']['consulta'] == 1) {
+                    //Envia la consulta al mail de cabañas.
+                    if($this->data['Contacto']['consulta'] == 0){
+                        $email->to('celeweidmann@gmail.com');
+                    } 
+                    //Envia la consulta al mail de Almacén Náutico
+                    elseif ($this->data['Contacto']['consulta'] == 1) {
                         $email->to('celeweidmann@hotmail.com');
-                } 
-     
-            }
-           
-            if ( $email->send($mensaje)){
-                 $this->Session->setFlash('<div class="alert alert-success"> Su consulta ha sido enviada con éxito.'.'<br>'.' 
+                    } 
+             
+                    if ( $email->send($mensaje)){
+                            //Mensaje indicando que la consulta se ha enviado con éxito.
+                            $this->Session->setFlash('<div class="alert alert-success"> Su consulta ha sido enviada con éxito.'.'<br>'.' 
                                             Nos comunicaremos con usted a la brevedad. </div>');
            
+                    }
+                }
             }
-        $this->redirect(array('controller' => 'contactos', 'action' => 'index'));
+ 
+        //$this->redirect(array('controller' => 'contactos', 'action' => 'index'));
 
         }
-
     }
 
-    
-     public function contacto(){
-
-        if ($this->request->is('post')) {
-                 
-            $email = new CakeEmail('gmail');
-                   
-            if(isset($this->data['Contacto'])){
-                
-                //Armo el mensaje para enviar al mail.   
-                $mensaje = 'Enviado por: '.$this->data['Contacto']['nombre']."\n".
-                           'Mail de contacto: '.$this->data['Contacto']['email']."\n".
-                           'Consulta: '.$this->data['Contacto']['message'];
-                
-                //Envia la consulta al mail de cabañas.
-                if($this->data['Contacto']['consulta'] == 0){
-                    $email->to('celeweidmann@gmail.com');
-                } 
-                //Envia la consulta al mail de Almacén Náutico
-                elseif ($this->data['Contacto']['consulta'] == 1) {
-                        $email->to('celeweidmann@hotmail.com');
-                } 
-     
-            }
-           
-            if ( $email->send($mensaje)){
-                 $this->Session->setFlash('<div class="alert alert-success"> Su consulta ha sido enviada con éxito.'.'<br>'.' 
-                                            Nos comunicaremos con usted a la brevedad. </div>');
-           
-            }
-        $this->redirect(array('controller' => 'contactos', 'action' => 'contacto'));
-
-        }
-     }
-     
-    public function respuesta() {}
+   
 
 }
