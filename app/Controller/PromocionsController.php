e79bc925daba2051dd23f8a3f96415ed178c1ced
cele <celeweidmann@gmail.com>
2012-12-11 18:00:33 -0300

    Iconos extra.

---
+++ app/Controller/PromocionsController.php
@@ -0,0 +1,13 @@
+<?php
+class PromocionesController extends AppController {
+    var $name = 'Promociones';
+    var $helpers = array('Html', 'Form');
+    
+    
+    public function index()
+    {
+        
+    }
+}
+    
+?>
